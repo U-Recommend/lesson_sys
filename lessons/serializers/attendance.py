d65940fa0bac4a785805bcb49539fc99952f37dd
tonytan <U_Recommend@51yjjf.com>
2022-12-09 20:47:51 +0800

    update

--- lessons/serializers/attendance.py
+++ lessons/serializers/attendance.py
@@ -15,7 +15,7 @@ def attendance_data(lesson=None, user=None):
     data = {
         'date': lesson.lesson_date.strftime("%Y-%m-%d") if lesson.lesson_date else "",
         'lesson_id': lesson.id,
-        'lesson_name': lesson.course.title,
+        'lesson_name': lesson.course.title if lesson.course else "",
     }
     logger.info(data)
     return data
